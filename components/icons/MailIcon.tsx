a5bb574ecb0214b926f167f9a2739c8037129dee
ARStech-lab <connect.arstech@gmail.com>
2025-10-19 20:55:23 +0530

    7th commit

--- components/icons/MailIcon.tsx
+++ components/icons/MailIcon.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
-const MailIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+const GmailIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="#D43B3B"
+    viewBox="0 0 32 32"
     aria-hidden="true"
     {...props}
   >
-    <path d="M22 5.889v12.222c0 .5-.327.94-.782 1.025-.05.01-.1.015-.15.015-.41 0-.78-.235-1.01-.585l-3.348-4.85-3.34-3.415 6.62-4.41zM2 5.889v12.222c0 .5.327.94.782 1.025.05.01.1.015.15.015.41 0 .78-.235 1.01-.585l3.348-4.85 3.34-3.415-6.62-4.41zM20.93.415l-8.62 5.745L3.715.415C3.365.175 2.945 0 2.5 0c-.065 0-.13.005-.195.015C1.83 1.1.985 1.735.985 2.445v.5L12 10.725 23.015 2.945v-.5c0-.71-.845-1.345-1.32-1.43C21.63 1.005 21.565 1 21.5 1c-.445 0-.865.175-1.215.415z"/>
+    {/* Blue left side of the "M" */}
+    <path fill="#4285F4" d="M4 26.6V9.3L16 18l-8.7 8.7c-.5.4-1.2.3-1.6-.3z" />
+    {/* Green right side of the "M" */}
+    <path fill="#34A853" d="M28 26.6V9.3L16 18l8.7 8.7c.5.4 1.2.3 1.6-.3z" />
+    {/* Yellow top-left shoulder of the "M" */}
+    <path
+      fill="#FBBC05"
+      d="M27.7 7.2c-.4-.5-1.1-.6-1.6-.3L16 14.8 5.9 6.9c-.5-.4-1.2-.3-1.6.3L2 8.4V7c0-1.7 1.3-3 3-3h22c1.7 0 3 1.3 3 3v1.4l-2.3-1.2z"
+    />
+    {/* Red top chevron of the "M" / envelope flap */}
+    <path
+      fill="#EA4335"
+      d="M29.4 8.6L16 19.2 2.6 8.6C2.2 8.2 2 7.6 2 7c0-1.7 1.3-3 3-3h22c1.7 0 3 1.3 3 3 0 .6-.2 1.2-.6 1.6z"
+    />
   </svg>
 );
 
-export default MailIcon;
+export default GmailIcon;
